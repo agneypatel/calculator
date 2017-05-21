98f98b18f1d28e7a92207b3a9dbce92b6b067310
agneypatel <agney999@gmail.com>
2017-05-21 16:03:37 +0530

    Add files via upload

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Agney Patel on 17/02/14.
+//  Copyright (c) 2014 Agney Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
